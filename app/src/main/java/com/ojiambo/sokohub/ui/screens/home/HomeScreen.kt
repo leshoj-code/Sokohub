ecbfaf8035e2115f9981c383343fc096100f692d
leshoj-code <leshyoj@gmail.com>
2026-04-02 12:02:26 +0300

    TopAppBar

--- app/src/main/java/com/ojiambo/sokohub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/ojiambo/sokohub/ui/screens/home/HomeScreen.kt
@@ -1,12 +1,25 @@
 package com.ojiambo.sokohub.ui.screens.home
 
+import android.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.ojiambo.sokohub.ui.theme.Burgundy
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(){
 
@@ -14,7 +27,34 @@ fun HomeScreen(){
         modifier = Modifier.fillMaxSize()
     ) {
 
-        Text( text = "Step in")
+        //TopAppBar
+        TopAppBar(
+            title = { Text(text = "Home", color = Color.White) },
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "menu")
+                }
+            },
+
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "cart")
+                }
+
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "notification")
+                }
+            },
+
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Burgundy,
+                navigationIconContentColor = Color.White,
+                actionIconContentColor = Color.White
+
+            )
+
+        )
+        //End of TopAppBar
 
     }
 
--- app/src/main/java/com/ojiambo/sokohub/ui/screens/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/ojiambo/sokohub/ui/screens/onboarding/OnboardingScreen.kt
@@ -1,21 +1,38 @@
 package com.ojiambo.sokohub.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ojiambo.sokohub.R
+import com.ojiambo.sokohub.ui.theme.Burgundy
 
 @Composable
 fun OnboardingScreen(){
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
 
         Image(
@@ -24,7 +41,40 @@ fun OnboardingScreen(){
             modifier = Modifier.size(300.dp)
 
 
-            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Welcome to Sokohub!!",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = FontFamily.SansSerif,
+            color = Color.Red
+        )
+
+        Text(
+            text = "Shop starter",
+            fontSize = 25.sp
+        )
+
+        Text(
+            text = "We provide a centralized platform for you to simplify daily operation such as managing orders, inventories and customer information across all sales channels.",
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(Burgundy),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(350.dp)
+        ) {
+            Text(text = "Get Started")
+        }
+
 
     }
 
