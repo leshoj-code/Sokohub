b4337babb79cbd952354d67dee70b457a7afb8d0
leshoj-code <leshyoj@gmail.com>
2026-04-22 13:12:00 +0300

    Products

--- app/src/main/java/com/ojiambo/sokohub/navigation/AppNavHost.kt
+++ app/src/main/java/com/ojiambo/sokohub/navigation/AppNavHost.kt
@@ -3,15 +3,20 @@ package com.ojiambo.sokohub.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ojiambo.sokohub.ui.screens.about.AboutScreen
 import com.ojiambo.sokohub.ui.screens.auth.LoginScreen
 import com.ojiambo.sokohub.ui.screens.auth.RegisterScreen
 import com.ojiambo.sokohub.ui.screens.home.HomeScreen
 import com.ojiambo.sokohub.ui.screens.intent.IntentScreen
 import com.ojiambo.sokohub.ui.screens.onboarding.OnboardingScreen
+import com.ojiambo.sokohub.ui.screens.products.AddProductScreen
+import com.ojiambo.sokohub.ui.screens.products.UpdateProductScreen
+import com.ojiambo.sokohub.ui.screens.products.ViewProductScreen
 import com.ojiambo.sokohub.ui.screens.scaffold.ScaffoldScreen
 import com.ojiambo.sokohub.ui.screens.service.ServiceScreen
 import com.ojiambo.sokohub.ui.screens.splash.SplashScreen
@@ -56,5 +61,19 @@ fun AppNavHost(
             ScaffoldScreen(navController)
         }
 
+        composable(ROUTE_ADD_PRODUCT) { AddProductScreen(navController) }
+
+        composable(ROUTE_VIEW_PRODUCTS) { ViewProductScreen(navController) }
+
+        composable(
+            ROUTE_UPDATE_PRODUCT,
+            arguments = listOf(navArgument("productId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val productId = backStackEntry.arguments?.getString("productId")!!
+            UpdateProductScreen(navController, productId)
+        }
+
     }
+
+
 }
